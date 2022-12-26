451ec799bc9fef0ee0c4bdce2ab7ebe9ad9d04aa
dimon4uk1990 <mexal07@ukr.net>
2022-12-26 15:03:51 +0200

    create hw05

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,23 @@
+const { User } = require('../../models/user');
+const path = require('path');
+const fs = require('fs/promises');
+
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
+const updateAvatar = async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+
+  const imageNeme = `${id}_${originalname}`;
+  try {
+    const resultUpload = path.join(avatarsDir, imageNeme);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('public', 'avatars', imageNeme);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
+};
+
+module.exports = updateAvatar;
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,10 +3,12 @@ const controllerWrapper = require('./controllerWrapper');
 const handleMongooseError = require('./handleMongooseError');
 const isValidId = require('./isValidId');
 const auth = require('./auth');
+const upload = require('./upload');
 module.exports = {
   validationBody,
   controllerWrapper,
   handleMongooseError,
   isValidId,
   auth,
+  upload,
 };
